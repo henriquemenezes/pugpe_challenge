c0a4f7b6b5b80ce470a4b1fac0431332e3c9d24d
Henrique Menezes <floresta.henrique@gmail.com>
2011-02-12 15:43:33 -0300

    Minha resposta

--- desafio_20110211/desafio.py
+++ desafio_20110211/desafio.py
@@ -1,41 +1,30 @@
 """
-Desafio PUG-PE  
+Desafio PUG-PE
 ID: 1
 Semana: 11/02/2011
+PUG: Henrique Menezes
+"""
 
-Problema:
 
-    Dado uma lista de elementos, o objetivo eh converter esta lista em uma lista de sub-listas de elementos consecutivos duplicados.
-    Assim que um elemento subsequente for diferente do anterior, a sublista eh gerada e passa para o proximo elemento. Entao 
-    no exemplo abaixo dado 5 a's quando encontrar um b ele gera a sublista de 5 a's e comeca a gerar a sublista de b's ate encontrar um
-    elemento que nao seja b, e assim sucessivamente. 
+def pack(iterable):
+    """
     >>> x = ['a','a','a','a','b','c','c','a','a','d','e','e','e','e']
     >>> ret = pack(x)
     >>> ret
-    >>> [['a','a','a','a'],['b'],['c','c'],['a','a'],['d'],['e','e','e','e']]
+    [['a', 'a', 'a', 'a'], ['b'], ['c', 'c'], ['a', 'a'], ['d'], ['e', \
+'e', 'e', 'e']]
     >>> x = ['a', 'b', 'c']
     >>> ret = pack(x)
-    >>> ret 
-    >>>[['a'], ['b'], ['c']]
+    >>> ret
+    [['a'], ['b'], ['c']]
     >>> x = []
     >>> ret = pack(x)
     >>> ret
-    >>> []
-     
-  Seu trabalho eh construir essa lista de elementos.  Favor utilizar Testes usando doctest ou UnitTest para validar sua solucao.
-
-"""
-
-import unittest
-
-
-class Desafio1(unittest.TestCase):
-
-    def test_pack_duplicates(self):
-        sampleList = ['a','a','a','a','b','c','c','a','a','d','e','e','e','e']
-        self.assertEqual([['a','a','a','a'],['b'],['c','c'],['a','a'],['d'],['e','e','e','e']],
-                    pack(sampleList))
-
+    []
+    """
+    import itertools
+    return [list(g) for k, g in itertools.groupby(iterable)]
 
 if __name__ == '__main__':
-    unittest.main()    
+    import doctest
+    doctest.testmod(verbose=True)
